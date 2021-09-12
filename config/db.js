a80f13ccfd768af5a67d3ca9921d573b1c97566d
gunratnamore911 <gunratnamore@gmail.com>
2021-09-12 19:16:48 +0530

    a

--- config/db.js
+++ config/db.js
@@ -8,7 +8,6 @@ const connectDb = async () => {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true,
     });
     console.log("Mongodb Connected".blue);
   } catch (err) {
