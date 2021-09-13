3849da8dcc05e20decfd0308ec224312f4c7583c
gunratnamore911 <gunratnamore@gmail.com>
2021-09-13 10:34:32 +0530

    add

--- config/db.js
+++ config/db.js
@@ -5,11 +5,7 @@ const colors = require("colors");
 
 const connectDb = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log(process.env.MONGO_URI);
     console.log("Mongodb Connected".blue);
   } catch (error) {
